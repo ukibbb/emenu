c37d53e4c79a61071bfb726ed83f36dd724d1e7f
ukibbb <lucas.bulinski@gmail.com>
2023-02-26 12:18:44 +0100

    updated settings and urls .py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,21 +1,25 @@
-"""core URL Configuration.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+API_VERSION = "api/v1/"
 
 urlpatterns = [
+    path(
+        f"{API_VERSION}token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        f"{API_VERSION}token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+    path(
+        f"{API_VERSION}accounts/",
+        include(("accounts.urls", "accounts"), namespace="accounts"),
+    ),
+    path(
+        f"{API_VERSION}menus/", include("menus")
+    )
     path("admin/", admin.site.urls),
 ]
